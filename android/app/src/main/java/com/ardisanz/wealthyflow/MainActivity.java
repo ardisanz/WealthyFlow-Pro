c580478753114f6b10de970e5c49e3d7ba4fce9b
ardisanz <pendie@gmail.com>
2026-05-14 10:13:40 +0700

    APK

---
+++ android/app/src/main/java/com/ardisanz/wealthyflow/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ardisanz.wealthyflow;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ www/sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'wealthyflow-v2';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Install Event');
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('[Service Worker] Caching all assets');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activate Event');
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[Service Worker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
