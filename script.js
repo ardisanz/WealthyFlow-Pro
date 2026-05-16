17c42e59c543db44e7e3e49ac5a0685d30755fad
ardisanz <pendie@gmail.com>
2026-05-16 10:42:42 +0700

    Header

--- script.js
+++ script.js
@@ -286,6 +286,13 @@ function moneyApp() {
             { value: '#7c3aed', label: 'Violet' }
         ],
 
+        getGreeting() {
+            const hour = new Date().getHours();
+            if (hour < 12) return 'Good Morning';
+            if (hour < 18) return 'Good Afternoon';
+            return 'Good Evening';
+        },
+
 
         // --- INITIALIZATION ---
         init() {
